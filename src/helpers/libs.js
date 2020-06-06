eb021676a3b6c97a59cd8449f6c1af4c37d7cbd9
Rdavid1991 <42180920+Rdavid1991@users.noreply.github.com>
2020-06-06 00:46:16 -0500

    Numero de caracteres para los nombres aleatorios

--- src/helpers/libs.js
+++ src/helpers/libs.js
@@ -1,8 +1,8 @@
 module.exports = {
-    randomName: () => {
+    randomName: (charNumber) => {
         const posible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         let randomName = 0;
-        for (let i = 0; i < 30; i++) {
+        for (let i = 0; i < charNumber; i++) {
             randomName += posible.charAt(Math.floor(Math.random() * posible.length));
         }
         return randomName;
