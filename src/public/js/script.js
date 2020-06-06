453b6776cc62f54033370e1f12927d1b6575a35b
Rdavid1991 <42180920+Rdavid1991@users.noreply.github.com>
2020-06-06 16:24:04 -0500

    Envio de datos para el video y funcion de campo requerido

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -13,41 +13,70 @@ function loadImage(image) {
     xhs.send();
 }
 
+/* htmlElements */
+let file = document.getElementById("file");
+let title = document.getElementById("title");
+let description = document.getElementById("description");
+
+let video = document.getElementById("video-preview");
+
+file.addEventListener("change", (e) => {
+    e.target.parentNode.parentNode.parentNode.classList.toggle("required-valid", false);
+});
+title.addEventListener("change", (e) => {
+    e.target.parentNode.classList.toggle("required-valid", false);
+});
+description.addEventListener("change", (e) => {
+    e.target.parentNode.classList.toggle("required-valid", false);
+});
 
 /* Subir archivos */
-document.getElementById("submit").addEventListener("click", (e) => {
-    e.target.disabled = true;
+function submit(button) {
     let progress = document.getElementById("progress-upload");
-    let file = document.getElementById("file").files[0];
-
     let data = new FormData();
-    data.append("file", file);
 
-    let xhs = new XMLHttpRequest();
-    xhs.open("POST", "/upload");
-    xhs.upload.addEventListener("progress", (e) => {
-        progress.ariaValueNow = e.loaded / e.total * 100;
-        progress.style.width = e.loaded / e.total * 100 + "%";
-    });
-    xhs.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let ext = String(xhs.response);
+    if (file.validity.valid && title.validity.valid && description.validity.valid) {
+        button.disabled = true;
+        data.append("file", file.files[0]);
+        data.append("title", title.value);
+        data.append("description", description.value);
 
-            let groupButton = document.getElementById("group-button");
-            if (ext.match(/.mp4/g)) {
-                groupButton.innerHTML = `<a href="/video/${xhs.response}" class="btn btn-success">Successful</a>`;
-            } else if (ext.match(/.jpg/g) || ext.match(/.png/g) || ext.match(/.jpeg/g) || ext.match(/.gif/g)) {
-                groupButton.innerHTML = `<a href="/images/${xhs.response}" class="btn btn-success">Successful</a>`;
-            } else if (ext.match(/.mp3/g) || ext.match(/.acc/g)) {
-                groupButton.innerHTML = `<a href="/sound/${xhs.response}" class="btn btn-success">Successful</a>`;
+        let xhs = new XMLHttpRequest();
+        xhs.open("POST", "/upload");
+        xhs.upload.addEventListener("progress", (e) => {
+            progress.ariaValueNow = e.loaded / e.total * 100;
+            progress.style.width = e.loaded / e.total * 100 + "%";
+        });
+        xhs.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                let ext = String(xhs.response);
+
+                let groupButton = document.getElementById("group-button");
+                if (ext.match(/.mp4/g)) {
+                    groupButton.innerHTML = `<a href="/video/${xhs.response}" class="btn btn-success">Successful</a>`;
+                } else if (ext.match(/.jpg/g) || ext.match(/.png/g) || ext.match(/.jpeg/g) || ext.match(/.gif/g)) {
+                    groupButton.innerHTML = `<a href="/images/${xhs.response}" class="btn btn-success">Successful</a>`;
+                } else if (ext.match(/.mp3/g) || ext.match(/.acc/g)) {
+                    groupButton.innerHTML = `<a href="/sound/${xhs.response}" class="btn btn-success">Successful</a>`;
+                }
+                openModal();
+            } else if (this.readyState == 4 && this.status != 200) {
+                console.log("facho");
             }
-            openModal();
-        } else if (this.readyState == 4 && this.status != 200) {
-            console.log("facho");
+        };
+        xhs.send(data);
+    } else {
+        if (!file.validity.valid) {
+            file.parentNode.parentNode.parentNode.classList.toggle("required-valid", true);
         }
-    };
-    xhs.send(data);
-});
+        if (!title.validity.valid) {
+            title.parentNode.classList.toggle("required-valid", true);
+        }
+        if (!description.validity.valid) {
+            description.parentNode.classList.toggle("required-valid", true);
+        }
+    }
+}
 
 /* Abrir modal */
 // Get the modal
@@ -58,11 +87,7 @@ function openModal() {
 }
 
 function watchPreview(miniature) {
-
-    console.log("funciona");
-    
-
-    var modal = document.getElementById("video-modal");
+    let modal = document.getElementById("video-modal");
     modal.style.display = "block";
 
     let xhs = new XMLHttpRequest();
@@ -71,8 +96,14 @@ function watchPreview(miniature) {
     xhs.onload = (e) => {
         let blob = new Blob([xhs.response]);
         let url = URL.createObjectURL(blob);
-        let video = document.getElementById("video-preview");
         video.src = url;
     };
     xhs.send();
+}
+
+function closeModal() {
+    let modal = document.getElementById("video-modal");
+    video.pause();
+    video.src="";
+    modal.style.display = "none";
 }
