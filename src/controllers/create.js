966e427f235d37d93ae4e14bd6fb93b656c5d9c7
Rdavid1991 <42180920+Rdavid1991@users.noreply.github.com>
2020-06-06 18:53:11 -0500

    Guardar metadata de video en base de datos

--- src/controllers/create.js
+++ src/controllers/create.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const fs = require('fs-extra');
 const Ffmpeg = require("fluent-ffmpeg");
+const moment = require("moment");
+
 const libs = require('../helpers/libs');
 const { Image, Video } = require('../models');
 
@@ -15,8 +17,10 @@ module.exports = {
             let imageUrl = await saveImage(ext, fileTempPath, req);
             res.send(imageUrl);
         } else if (ext === '.mp4') {
+
             let videoUrl = await saveVideo(ext, fileTempPath, req);
             res.send(videoUrl);
+
         } else if (ext === '.mp3' || ext === ".acc") {
             let imageUrl = await saveAudio(ext, fileTempPath, req);
             res.send(imageUrl);
@@ -58,40 +62,77 @@ const saveVideo = async (ext, videoTempPath, req) => {
     if (video.length > 0) {
         saveImage();
     } else {
+
         const targetPath = path.resolve(`src/upload/video/${videoUrl}${ext}`);
-        if (!fs.existsSync("src/upload/video/")){
-            fs.mkdirSync("src/upload/video/");
-        }
-
         await fs.rename(videoTempPath, targetPath);
-        const newVideo = new Video({
-            title: req.body.title,
-            filename: videoUrl + ext,
-            miniature: miniature,
-            description: req.body.description,
-        });
-
-        newVideo.save();
 
         Ffmpeg.setFfmpegPath("C:/Users/david/Desktop/bin/ffmpeg.exe");
         Ffmpeg.setFfprobePath("C:/Users/david/Desktop/bin/ffprobe.exe");
 
         const miniatureRoute = 'src/upload/video-miniature';
 
-        if (!fs.existsSync(miniatureRoute)){
+        if (!fs.existsSync(miniatureRoute)) {
             fs.mkdirSync(miniatureRoute);
         }
 
         const ffmpeg = Ffmpeg;
         ffmpeg(targetPath)
-        .screenshots({
-            count: 1,
-            filename: miniature,
-            folder:miniatureRoute ,
-            size: '320x240'
-          });
+            .screenshots({
+                count: 1,
+                filename: miniature,
+                folder: miniatureRoute,
+                size: '320x240'
+            });
 
-        return videoUrl;
+        let metaData = (targetPath) => new Promise((res, rej) => {
+
+            ffmpeg.ffprobe(targetPath, (err, data) => {
+                let hour, min, seg, width, heigth;
+
+                /* Duracion del video */
+                let duration = Math.floor(data.format.duration * 1000);
+                let d = moment.duration(duration, 'milliseconds');
+
+                hour = Math.floor(d.asHours());
+                min = Math.floor(d.asMinutes()) - hour * 60;
+                seg = Math.floor(d.asSeconds()) - min * 60;
+
+                /* Resolucion de video */
+                width = data.streams[0].width;
+                heigth = data.streams[0].height;
+
+                res({ hour, min, seg, width, heigth });
+            });
+        });
+
+        let data = await metaData(targetPath);
+
+        if (!fs.existsSync("src/upload/video/")) {
+            fs.mkdirSync("src/upload/video/");
+        }
+
+        console.log(typeof(data.min));
+        
+
+        if (data.min < 5) {
+            const newVideo = new Video({
+                title: req.body.title,
+                filename: videoUrl + ext,
+                min: data.min,
+                seg: data.seg,
+                width: data.width,
+                Height: data.heigth,
+                miniature: miniature,
+                description: req.body.description,
+            });
+            newVideo.save();
+            return { videoUrl, dutation: null };
+        } else {
+            setTimeout(()=>{
+                fs.unlink(targetPath);
+            },5000);
+            return { videoUrl: null, dutation: "Exede los 5 minutos" };
+        }
     }
 };
 
